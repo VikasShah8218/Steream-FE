cf8b2f8922fb56fe00b724e55fbb0aa021f4f07e
Vikas shah <vikasshah8218@gmail.com>
2025-04-05 19:12:37 +0530

    ok

--- src/globals/requests.tsx
+++ src/globals/requests.tsx
@@ -3,7 +3,8 @@ import { getAuthToken } from "./auth";
 import store from "../app/store";
 import { logout } from "../app/slices/authSlice";
 
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "http://13.49.145.155:8000";
 
 const ERROR_MSG = { type: "error", text: "Something went wrong." };
 
